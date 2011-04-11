5ec87c2a0d36c90c5b15e80d61d50c5944f7b83d
Jeffrey Horn <jrhorn424@gmail.com>
2011-04-11 12:06:20 -0400

    Added error handling to UTF-8 encoding

--- scraper.py
+++ scraper.py
@@ -20,7 +20,11 @@ for rec in range(1, maxrec, 25):
     data = urllib2.urlopen("http://www.charitynavigator.org/feeds/search4/?appid=%d&fromrec=%d" % (appid, rec)) 
     doc = lxml.etree.parse(data)
     for charity in doc.findall('charity'):
-        results.append(dict((item.tag, item.text.encode('utf-8')) for item in charity.iterchildren()))
+        try:
+            results.append(dict((item.tag, item.text.encode('utf-8')) for item in charity.iterchildren()))
+        except:
+            print "Exception (probably utf-8 encoder)"
+            results.append(dict((item.tag, item.text) for item in charity.iterchildren()))
     # with open('xml/%05d.xml' % rec,'wb') as f:
     #       f.write(data.read()) # save a copy, just in case
     data.close()
@@ -30,7 +34,11 @@ for charity in results:
     doc = lxml.html.parse(urllib2.urlopen(charity['url'],timeout=600))
     for element in doc.xpath('//div/table/tr/td/a'):
         if element.text=='Fundraising Efficiency':
-            rating = element.getparent().getnext().text.encode('utf-8')
+            try:
+                rating = element.getparent().getnext().text.encode('utf-8')
+            except:
+                print "Exception (probably utf-8 encoder)"
+                rating = element.getparent().getnext().text
     charity['efficiency'] = rating
     # with open('html/$05d.html' % charity['orgid'], 'wb') as f:
     #     f.write(data.read())
